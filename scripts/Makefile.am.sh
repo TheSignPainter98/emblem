cba44d548326e934851eef2d0c7d008a839c8162
Ed Jones <edjones98@gmail.com>
2020-06-12 17:42:19 +0100

    Added testing

--- scripts/Makefile.am.sh
+++ scripts/Makefile.am.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-srcs=$(echo ./src/argp.c ./src/argp.h $(find -name '*.c' -or -name '*.h' | grep -v 'argp.c$' | grep -v 'argp.h$') | sed 's/\//\\\//g')
+srcs=$(echo ./src/argp.c ./src/argp.h $(find src -name '*.c' -or -name '*.h' | grep -v 'argp.c$' | grep -v 'argp.h$') | sed 's/\//\\\//g')
+tests=$(echo ./src/argp.c ./src/argp.h $(find src -name '*.c' -or -name '*.h' | grep -v 'argp.c$' | grep -v 'argp.h$' | grep -v 'em.c$' | grep -v 'em.h$') $(find check -name '*.c' -or -name '*.h') | sed 's/\//\\\//g')
 
 echo $srcs
-sed "s/<SRC_FILES>/$srcs/" < /dev/stdin > /dev/stdout
+(sed "s/<SRC_FILES>/$srcs/" \
+	| sed "s/<TEST_FILES>/$tests/"\
+	) < /dev/stdin > /dev/stdout
