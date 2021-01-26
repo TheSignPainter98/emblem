a0abb26b21eb000c0266ace530d1698eb6708bae
Ed Jones <edjones98@gmail.com>
2021-01-26 21:23:16 +0000

    Improve handling of built source files

--- scripts/Makefile.am.sh
+++ scripts/Makefile.am.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-extra_srcs=(./src/argp.c ./src/argp.h ./src/pp/ignore_warning.h)
-srcs=$(echo ${extra_srcs[@]} $(find src -name '*.c' -or -name '*.h' | grep -v 'argp.c$' | grep -v 'argp.h$') | sed 's/\//\\\//g')
+built_srcs=(./src/argp.c ./src/argp.h ./src/pp/ignore_warning.h)
+sanitised_built_srcs=$(echo ${built_srcs[@]} | sed 's/\//\\\//g')
+srcs=$(echo ${built_srcs[@]} $(find src -name '*.c' -or -name '*.h' | grep -v 'argp.c$' | grep -v 'argp.h$' | grep -v 'src/pp/ignore_warning.h$') | sed 's/\//\\\//g')
 tests=$(echo ./src/argp.c ./src/argp.h $(find src -name '*.c' -or -name '*.h' | grep -v 'argp.c$' | grep -v 'argp.h$' | grep -v 'em.c$' | grep -v 'em.h$') $(find check -name '*.c' -or -name '*.h') | sed 's/\//\\\//g')
 
-echo $srcs
 (sed "s/<SRC_FILES>/$srcs/" \
 	| sed "s/<TEST_FILES>/$tests/"\
+	| sed "s/<BUILT_SRCS>/$sanitised_built_srcs/"\
 	) < /dev/stdin > /dev/stdout
