4d5b13b1664d19fd4cef43f30dfa09c31fdc7451
Ed Jones <edjones98@gmail.com>
2021-01-26 20:59:39 +0000

    Added minor testing

--- src/em.c
+++ src/em.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 #include "argp.h"
+#include "logs/logs.h"
 
 /**
  * @brief Entry point
@@ -13,14 +14,18 @@
 int main(int argc, char** argv)
 {
 	// Parse arguments, exit if necessary
-	int rc = parse_args(argc, argv);
+	Args args;
+	int rc = parse_args(&args, argc, argv);
 	if (rc)
 		return rc;
 
-	printf("Verbosity is %d\n", Verbose);
-	printf("Style = %s\n", Style);
-	printf("DefaultTypeface = %s\n", DefaultTypeface);
-	printf("DefaultFontSize = %d\n", DefaultFontSize);
+	init_logs(&args);
+	log_err("Hello, world %s", "how are you?");
+	log_warn("Hello, world %s", "how are you?");
+	log_info("Hello, world %s", "how are you?");
+	log_succ("Hello, world %s", "how are you?");
+
+	log_succ("Got default font %s @ %dpt", args.default_typeface, args.default_font_size);
 
 	return 0;
 }
