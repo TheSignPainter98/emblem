a18433fdc74d6247d298aa21077e2f0433214e2a
Ed Jones <edjones98@gmail.com>
2021-01-26 20:56:40 +0000

    Updates and changes

--- scripts/Makefile.am.sh
+++ scripts/Makefile.am.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-srcs=$(echo ./src/argp.c ./src/argp.h $(find src -name '*.c' -or -name '*.h' | grep -v 'argp.c$' | grep -v 'argp.h$') | sed 's/\//\\\//g')
+extra_srcs=(./src/argp.c ./src/argp.h ./src/pp/ignore_warning.h)
+srcs=$(echo ${extra_srcs[@]} $(find src -name '*.c' -or -name '*.h' | grep -v 'argp.c$' | grep -v 'argp.h$') | sed 's/\//\\\//g')
 tests=$(echo ./src/argp.c ./src/argp.h $(find src -name '*.c' -or -name '*.h' | grep -v 'argp.c$' | grep -v 'argp.h$' | grep -v 'em.c$' | grep -v 'em.h$') $(find check -name '*.c' -or -name '*.h') | sed 's/\//\\\//g')
 
 echo $srcs
