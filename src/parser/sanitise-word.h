9d3ad30dd43b0d3808488bf56ecf6dddf4c51cf0
Ed Jones <edjones98@gmail.com>
2021-05-17 01:31:59 +0100

    Generalised and unified sanitisation data structures

--- src/parser/sanitise-word.h
+++ src/parser/sanitise-word.h
@@ -1,5 +1,8 @@
 #pragma once
 
+#include "data/str.h"
+#include "doc-struct/location.h"
+#include "emblem-parser.h"
 #include <stddef.h>
 
-char* sanitise_word(char* word, size_t len) __attribute__((hot));
+char* sanitise_word(EM_LTYPE* loc, Str* ifn, char* word, size_t len) __attribute__((hot));
