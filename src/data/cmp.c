a270d26b950cc39008462b5a92967e1ea51f855a
Ed Jones <edjones98@gmail.com>
2021-06-02 20:17:36 +0100

    Added clang-friendly comparison function implementations

--- src/data/cmp.c
+++ src/data/cmp.c
@@ -4,8 +4,14 @@
 #include "pp/ignore_warning.h"
 #include <string.h>
 
+#define COMPARISON_BODY(type)                                                                                                \
+	{                                                                                                                  \
+		TYPE_PUN_DEREFERENCE(type p1 = *(type*)&v1);                                                                   \
+		TYPE_PUN_DEREFERENCE(type p2 = *(type*)&v2);                                                                   \
+		return p1 < p2 ? CMP_LT : p1 == p2 ? CMP_EQ : CMP_GT;                                                          \
+	}
 #if __clang__
-#	define CMP(name, type)
+#	define CMP(name, type) Cmp(fun cmp_##name##s)(void*, void*) = fun Cmp(void* v1, void* v2) COMPARISON_BODY(type);
 #elif __GNUC__
 /**
  * @brief Create a generic comparator function
@@ -15,13 +21,7 @@
  *
  * @return A function which compares two `type`s
  */
-#	define CMP(name, type)                                                                                            \
-		Cmp cmp_##name##s(void* v1, void* v2)                                                                          \
-		{                                                                                                              \
-			TYPE_PUN_DEREFERENCE(type p1 = *(type*)&v1);                                                               \
-			TYPE_PUN_DEREFERENCE(type p2 = *(type*)&v2);                                                               \
-			return p1 < p2 ? CMP_LT : p1 == p2 ? CMP_EQ : CMP_GT;                                                      \
-		}
+#	define CMP(name, type) Cmp cmp_##name##s(void* v1, void* v2) COMPARISON_BODY(type)
 #endif
 
 CMP(char, char)
