a1f977b479e01d69e002cc2cc610d3c92edea2d3
Ed Jones <edjones98@gmail.com>
2021-06-02 20:14:45 +0100

    Correctly make missing files

--- scripts/clang-tidy-info.sh
+++ scripts/clang-tidy-info.sh
@@ -2,7 +2,7 @@
 
 (echo -n "["; \
 for file in $(./scripts/lintable-srcs.sh); do
-	[[ ! -f $file ]] && make file >&2
+	[[ ! -f $file ]] && make $file >&2
 	awk -f scripts/linting-line-filter.awk -v file=$file < $file
 done | sed 's/,$//';
 echo -n "]") > clang-tidy-info.json
