ddf3661df43fce7dcd4bd173fe992ae047585bdc
Ed Jones <edjones98@gmail.com>
2021-08-11 21:43:23 +0100

    Added linting to standard extension library

--- scripts/lintable-srcs.sh
+++ scripts/lintable-srcs.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 echo ./src/pp/ignore_warning.h ./src/parser/emblem-parser.h ./src/argp.c $(find src -name '*.c')
+find src/ext/lib -name '*.moon' | grep -v 'base.moon$' | tr '\n' ' '
+echo
