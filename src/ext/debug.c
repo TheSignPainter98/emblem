7e879dabf0a2fa5f4adb591cd6b389dfe5309fbd
Ed Jones <edjones98@gmail.com>
2021-08-16 14:16:47 +0100

    `dumpstack` outputs to stderr on one line

--- src/ext/debug.c
+++ src/ext/debug.c
@@ -8,27 +8,27 @@ void _dumpstack(lua_State* L)
 {
 	int top = lua_gettop(L);
 	if (!top)
-		log_debug("|> <>");
+		fputs("|> <>\n", stderr);
 	else
 		for (int i = 1; i <= top; i++)
 		{
-			log_debug("|> %d\t%s:\t", i, luaL_typename(L, i));
+			fprintf(stderr, "|> %d\t%s:\t", i, luaL_typename(L, i));
 			switch (lua_type(L, i))
 			{
 				case LUA_TNUMBER:
-					log_debug("\033[1A\t\t\t\t\t\t\t%g", lua_tonumber(L, i));
+					fprintf(stderr, "%g\n", lua_tonumber(L, i));
 					break;
 				case LUA_TSTRING:
-					log_debug("\033[1A\t\t\t\t\t\t\t%s", lua_tostring(L, i));
+					fprintf(stderr, "'%s'\n", lua_tostring(L, i));
 					break;
 				case LUA_TBOOLEAN:
-					log_debug("\033[1A\t\t\t\t\t\t\t%s", (lua_toboolean(L, i) ? "true" : "false"));
+					fprintf(stderr, "%s\n", (lua_toboolean(L, i) ? "true" : "false"));
 					break;
 				case LUA_TNIL:
-					log_debug("\033[1A\t\t\t\t\t\t\t%s", "");
+					fprintf(stderr, "%s\n", "");
 					break;
 				default:
-					log_debug("\033[1A\t\t\t\t\t\t%s", luaL_tolstring(L, i, NULL));
+					fprintf(stderr, "'%s'\n", luaL_tolstring(L, i, NULL));
 					lua_pop(L, 1);
 					break;
 			}
