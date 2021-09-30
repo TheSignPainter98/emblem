b42424163b523aae64118f44db1a622fc783326f
Ed Jones <edjones98@gmail.com>
2021-09-30 17:09:23 +0100

    Added sans/serif toggle button

--- docs/theme/js/h1-dedent.js
+++ docs/theme/js/h1-dedent.js
@@ -1,7 +1,17 @@
 h1s = Array.from(document.getElementsByTagName('h1'));
 
-h1s.forEach((h1,_) => {
+h1s.forEach((h1) => {
 	code = Array.from(h1.getElementsByTagName('code'));
 	if (code.length && code[0].textContent.charAt(0) == '.')
 		h1.classList.add('directive');
 });
+
+root = document.getElementsByTagName('body')[0];
+is_sans = false;
+toggleSans = () => {
+	is_sans = !is_sans;
+	if (is_sans)
+		root.classList.add('serif');
+	else
+		root.classList.remove('serif');
+};
