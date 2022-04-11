63285ca51d5468f47914122804045bd890bf1b33
Ed Jones <edjones98@gmail.com>
2022-04-11 17:14:12 +0100

    Re-implemented UniqueID as a Lua integer

--- src/data/unique-id.c
+++ src/data/unique-id.c
@@ -2,16 +2,13 @@
 
 #include "logs/logs.h"
 #include "pp/assert.h"
-#include <lualib.h>
-
-static void check_ext_int_widths(void) __attribute__((constructor));
-static void check_ext_int_widths(void) { ASSERT(sizeof(lua_Integer) >= sizeof(void*)); }
 
 static UniqueID id = (UniqueID)1;
 
 UniqueID get_unique_id(void)
 {
-	POINTER_ARITH(if (id) return id++;);
+	if (id)
+		return id++;
 	log_err("Too many objects, ID uniqueness guarantee has been lost");
 	exit(1);
 }
--- src/data/unique-id.h
+++ src/data/unique-id.h
@@ -1,7 +1,8 @@
 #pragma once
 
 #include <stddef.h>
+#include <lualib.h>
 
-typedef void* UniqueID;
+typedef lua_Integer UniqueID;
 
 UniqueID get_unique_id(void);
