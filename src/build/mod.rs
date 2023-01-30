c9a5179a4e0fd1934eb4ebe257f376d5ef58df52
Ed Jones <ed.jones@canonical.com>
2023-01-30 20:07:40 +0000

    Use logging macros for errors

--- src/build/mod.rs
+++ src/build/mod.rs
@@ -1,15 +1,23 @@
 use crate::args::{BuildCmd, SearchResult};
 use crate::context::Context;
-use crate::parser;
-use std::error::Error;
+use crate::log::{Log, Src};
+use crate::parser::{self, Error};
+use std::error;
 
-pub fn build(cmd: BuildCmd) -> Result<(), Box<dyn Error>> {
+pub fn build(cmd: BuildCmd) -> Result<(), Box<dyn error::Error>> {
     let mut ctx = Context::new();
 
     let fname = SearchResult::try_from(&cmd.input.file)?;
+
     match parser::parse_file(&mut ctx, fname) {
         Ok(d) => println!("{:?}", d),
-        Err(e) => println!("{}", e),
+        Err(e) => {
+            match *e {
+                Error::StringConversion(e) => alert!(Log::error(&e.to_string())),
+                Error::Filesystem(e) => alert!(Log::error(&e.to_string())),
+                Error::Parse(e) =>  alert!(Log::error(&e.to_string()))
+            }
+        }
     }
 
     Ok(())
--- src/main.rs
+++ src/main.rs
@@ -11,19 +11,22 @@ mod repo;
 
 use args::{Args, Command};
 use std::process::ExitCode;
+use log::Log;
 
 fn main() -> ExitCode {
     let args = Args::parse();
     log::init(args.log);
 
-    match args.command {
+    let ret = match args.command {
         Command::Build(args) => build::build(args),
         Command::Format(_) => panic!("fmt not implemented"),
         Command::Init(args) => init::init(args),
         Command::Lint(_) => panic!("lint not implemented"),
         Command::List(_) => panic!("list not implemented"),
+    };
+    if let Err(e) = ret {
+        alert!(Log::error(&e.to_string()));
     }
-    .unwrap();
 
     log::report()
 }
