670d1ac04bb94d02f2db01a505433768261f29cb
Ed Jones <ed.jones@canonical.com>
2023-01-31 01:24:50 +0000

    Compute spans of locations

--- src/parser/location.rs
+++ src/parser/location.rs
@@ -42,6 +42,32 @@ impl<'i> Location<'i> {
     pub fn indices(&self) -> (usize, usize) {
         self.indices
     }
+
+    pub fn span_to(&self, other: &Self) -> Self {
+        if self.file_name != other.file_name {
+            panic!(
+                "internal error: attempted to span across files: {} and {}",
+                self.file_name, other.file_name
+            );
+        }
+
+        Self {
+            file_name: other.file_name,
+            src: self.src,
+            lines: (
+                cmp::min(self.lines.0, other.lines.0),
+                cmp::max(self.lines.1, other.lines.1),
+            ),
+            indices: (
+                cmp::min(self.indices.0, other.indices.0),
+                cmp::max(self.indices.1, other.indices.1),
+            ),
+            cols: (
+                cmp::min(self.cols.0, other.cols.0),
+                cmp::max(self.cols.1, other.cols.1),
+            ),
+        }
+    }
 }
 
 impl Display for Location<'_> {
@@ -96,5 +122,38 @@ mod test {
         assert_eq!((start.line, end.line), loc.lines());
         assert_eq!((start.col, 1), loc.cols());
         assert_eq!((start.index, end.index), loc.indices());
+    }
+
+    #[test]
+    fn span_to() {
+        let text = "my name is methos\n";
+        let p1 = Point::new("fname.em", text);
+        let p2 = p1.clone().shift("my name");
+        let p3 = p2.clone().shift(" is ");
+        let p4 = p2.clone().shift("methos");
+
+        for (l1, l2) in vec![
+            (Location::new(&p1, &p2), Location::new(&p3, &p4)),
+            (Location::new(&p1, &p3), Location::new(&p2, &p4)),
+            (Location::new(&p1, &p4), Location::new(&p2, &p3)),
+        ] {
+            for (l1, l2) in vec![(&l1, &l2), (&l2, &l1)] {
+                let span = l1.span_to(l2);
+                assert_eq!(
+                    span.indices.0,
+                    cmp::min(
+                        cmp::min(l1.indices.0, l1.indices.1),
+                        cmp::min(l2.indices.0, l2.indices.1)
+                    )
+                );
+                assert_eq!(
+                    span.indices.1,
+                    cmp::max(
+                        cmp::max(l1.indices.0, l1.indices.1),
+                        cmp::max(l2.indices.0, l2.indices.1)
+                    )
+                );
+            }
+        }
     }
 }
