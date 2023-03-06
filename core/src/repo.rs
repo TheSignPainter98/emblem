ed514b9c9b4e4ee3f8bbe004a6e94efe03dd66f1
Ed Jones <ed.jones@canonical.com>
2023-03-06 20:52:16 +0000

    Make `git2` dependency optional

--- core/src/repo.rs
+++ core/src/repo.rs
@@ -1,13 +1,24 @@
-use git2::{Error, ErrorCode, Repository, Status, StatusOptions};
+#[cfg(feature = "git2")]
+use git2::{ErrorCode, Repository, Status, StatusOptions};
+use std::error::Error;
 use std::path::Path;
 
-/// Check whether there is a dirty repository at the given path.
+#[cfg(not(feature = "git2"))]
 #[allow(dead_code)]
-pub fn is_dirty(dir: &Path) -> Result<bool, Error> {
+pub fn is_dirty(_dir: &Path) -> Result<bool, Box<dyn Error>> {
+    panic!(
+        "internal error: emblem_core::repo::is_dirty cannot be called without the 'git2' feature"
+    );
+}
+
+/// Check whether there is a dirty repository at the given path.
+#[cfg(feature = "git2")]
+#[allow(dead_code)]
+pub fn is_dirty(dir: &Path) -> Result<bool, Box<dyn Error>> {
     let repo = match Repository::open(dir) {
         Ok(r) => r,
         Err(e) if e.code() == ErrorCode::NotFound => return Ok(false),
-        Err(e) => return Err(e),
+        Err(e) => return Err(Box::new(e)),
     };
 
     let mut opts = StatusOptions::new();
@@ -36,26 +47,28 @@ pub fn is_dirty(dir: &Path) -> Result<bool, Error> {
 
 #[cfg(test)]
 mod test {
-    use std::{error::Error, fs::File, io::Write};
+    use super::*;
+    use std::{fs::File, io::Write};
 
     use git2::{Repository, RepositoryInitOptions};
 
+    #[cfg(feature = "git2")]
     #[test]
-    fn is_dirty() -> Result<(), Box<dyn Error>> {
+    fn dirt_detection() -> Result<(), Box<dyn Error>> {
         let dir = tempfile::tempdir()?;
 
-        assert!(!super::is_dirty(dir.path())?);
+        assert!(!is_dirty(dir.path())?);
 
         Repository::init_opts(dir.path(), RepositoryInitOptions::new().mkdir(true))?;
 
-        assert!(!super::is_dirty(dir.path())?);
+        assert!(!is_dirty(dir.path())?);
 
         {
             let mut file = File::create(dir.path().join("dirt.txt"))?;
             file.write_all(b"some dirt")?;
         }
 
-        assert!(super::is_dirty(dir.path())?);
+        assert!(is_dirty(dir.path())?);
 
         Ok(())
     }
