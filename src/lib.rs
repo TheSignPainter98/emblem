016116320a513917133b1f6478d08a5688780a5d
Ed Jones <ed.jones@canonical.com>
2023-03-06 11:45:34 +0000

    Add note to `lib.rs` stub

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,5 @@
-
+// Welcome!
+//
+// If you're reading this, you're probably either looking for the emblem core or the emblem CLI.
+// For the former, search for other files named 'lib.rs' elsewhere in the repo and for the latter, search for
+// 'main.rs'.
