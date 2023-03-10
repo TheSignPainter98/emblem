d13158ded5b47ceebe1420660c3c2ba72d46651f
Ed Jones <ed.jones@canonical.com>
2023-03-10 23:13:56 +0000

    Fix testing on unexpected heading log

--- crates/emblem_core/src/log/messages/unexpected_heading.rs
+++ crates/emblem_core/src/log/messages/unexpected_heading.rs
@@ -9,14 +9,8 @@ pub struct UnexpectedHeading<'i> {
 }
 
 impl<'i> Message<'i> for UnexpectedHeading<'i> {
-    fn id() -> &'static str {
-        "E004"
-    }
-
     fn log(self) -> Log<'i> {
         Log::error("unexpected heading")
-            .with_id(Self::id())
-            .explainable()
             .with_src(Src::new(&self.loc).with_annotation(Note::error(&self.loc, "found here")))
             .with_help("headings should only appear at the start of lines")
     }
