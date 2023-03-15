612ead18d617a31f5b7e53c3a2a04501aa757ef0
Ed Jones <edjones98@gmail.com>
2023-03-15 19:20:26 +0000

    Formatting fixes

--- crates/emblem_core/src/log/messages/unexpected_token.rs
+++ crates/emblem_core/src/log/messages/unexpected_token.rs
@@ -26,10 +26,7 @@ impl<'i> Message<'i> for UnexpectedToken<'i> {
             return UnexpectedEOF::new(self.loc.end(), vec![]).log();
         }
         Log::error(format!("unexpected {}", self.token))
-            .with_src(
-                Src::new(&self.loc)
-                    .with_annotation(Note::error(&self.loc, "found here")),
-            )
+            .with_src(Src::new(&self.loc).with_annotation(Note::error(&self.loc, "found here")))
             .with_expected(self.expected)
     }
 }
