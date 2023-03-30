27f7637c6b016db10ce73ccbdad6eca929eeaa79
Ed Jones <ed.jones@canonical.com>
2023-03-30 20:33:53 +0100

    Check for cycles, emit single binary chunk with tests if required

--- crates/yuescript/src/compile.lua
+++ crates/yuescript/src/compile.lua
@@ -1,76 +1,213 @@
--- package.path = package.path .. ';crates/yuescript/luacheck/src/?/init.lua'
-package.path = package.path .. ';/home/kcza/Documents/projects/emblem/crates/yuescript/luacheck/src/?/init.lua'
-package.path = package.path .. ';/home/kcza/Documents/projects/emblem/crates/yuescript/luacheck/src/?.lua'
+package.path = package.path .. ';' .. luacheck_path
 
 local yue = require('yue')
 local luacheck = require('luacheck')
 
-function die(msg)
+local function show(v)
+	local t = type(v)
+	if t == 'boolean' or t == 'nil' or t == 'number' then
+		return tostring(v)
+	elseif t == 'function' or t == 'userdata' or t == 'thread' then
+		return '(' .. tostring(v) .. ')'
+	elseif t == 'string' then
+		return "'" .. v .. "'"
+	elseif t ~= 'table' then
+		error('unknown type ' .. t .. ' encountered')
+	end
+
+	local is_list = true
+	local maxk = -1
+	for k, _ in pairs(v) do
+		if type(k) ~= 'number' then
+			is_list = false
+			break
+		end
+		if maxk < k then
+			maxk = k
+		end
+	end
+	is_list = is_list and maxk == #v
+
+	if is_list then
+		local buf = { '[' }
+		for i = 1, #v do
+			if i > 1 then
+				buf[#buf + 1] = ', '
+			end
+			buf[#buf + 1] = show(v[i])
+		end
+		buf[#buf + 1] = ']'
+		return table.concat(buf)
+	end
+
+	local buf = { '{' }
+	for k, v2 in pairs(v) do
+		if #buf > 2 then
+			buf[#buf + 1] = ', '
+		end
+		buf[#buf + 1] = show(k)
+		buf[#buf + 1] = ': '
+		buf[#buf + 1] = show(v2)
+	end
+	buf[#buf + 1] = '}'
+	return table.concat(buf)
+end
+
+local function die(msg)
 	if msg then
-		print("===============================================================")
+		print('===============================================================')
+		if type(msg) ~= 'string' then
+			msg = show(msg)
+		end
 		print(msg)
-		print("===============================================================")
+		print('===============================================================')
 	end
 	os.exit(1)
 end
 
--- input:
---     find files
---     figure out dependencies and hence load order
-function compile(module_name, raw, test)
-	local script
-	if test then
-		script = dedent([[
-			macro tests = (t) -> table.concat {
-				"local tests = #{t}",
-				"tests!",
-			}, '\n'
-		]]) .. raw
-	else
-		script = 'macro busted = (t) -> ""\n' .. raw
-	end
-
-	local lua = yuescript_to_lua(module_name, script)
-
-	lint(module_name, lua)
-
-	-- TODO(kcza): typecheck with teal?
-
-	if not test then
-		return encode(string.dump(load(lua)))
-	else
-		return encode(lua)
-	end
-end
-
-function dedent(string)
+local function dedent(string)
 	local lines = {}
 	local indent
-	for line in string.gmatch(string, "([^\r\n]*)[\n\r]?") do
+	for line in string.gmatch(string, '([^\r\n]*)[\r\n]?') do
 		if not indent then
-			indent = '^' .. string.match(line, "^%s+")
+			indent = '^' .. string.match(line, '^%s+')
 		end
-		lines[#lines + 1] = string.gsub(line, indent, "")
+		lines[#lines + 1] = string.gsub(line, indent, '')
 	end
-	return table.concat(lines, "\n")
+	return table.concat(lines, '\n')
 end
 
-function yuescript_to_lua(name, script)
-	local lua, err, globals = yue.to_lua(script, {
-		implicit_return_root = true,
-		reserve_line_number = true,
-		reserve_comment = true,
-		lint_global = true,
-		module = name,
-		target = '5.1',
-	})
-	if err then
-		die(err)
+local function prepare(inputs)
+	local prepared = {}
+	for module, raw in pairs(inputs) do
+		if test then
+			prepared[module] =
+				table.concat({
+					'macro tests = (t) -> "table.insert __tests, #{t}"\n',
+					raw,
+				}), '\n'
+		else
+			prepared[module] = 'macro tests = (t) -> ""\n' .. raw
+		end
 	end
-	return lua
+	return prepared
 end
 
-function lint(module_name, lua)
+local function asts_of(inputs)
+	local asts = {}
+
+	for module, raw in pairs(inputs) do
+		local ast, err = yue.to_ast(raw)
+		if err then
+			die(err)
+		end
+		asts[module] = ast
+	end
+
+	return asts
+end
+
+local function luas_of(inputs)
+	local luas = {}
+	for module, raw in pairs(inputs) do
+		local lua, err, globals = yue.to_lua(raw, {
+			implicit_return_root = true,
+			reserve_line_number = true,
+			reserve_comment = true,
+			lint_global = true,
+			module = module,
+			target = '5.1',
+		})
+		if err then
+			die(err)
+		end
+		luas[module] = lua
+	end
+	return luas
+end
+
+function dfs(name, arcs, on_node, on_cycle, stack)
+	if stack == nil then
+		stack = {}
+	end
+
+	for i = 1, #stack do
+		if name == stack[i] then
+			if on_cycle then
+				on_cycle(stack)
+			end
+			return
+		end
+	end
+
+	stack[#stack + 1] = name
+
+	if on_node then
+		if on_node(name) == false then
+			return
+		end
+	end
+
+	local nexts = arcs[name]
+	if nexts then
+		for i = 1, #nexts do
+			dfs(nexts[i], arcs, on_node, on_cycle, stack)
+		end
+	end
+	stack[#stack] = nil
+end
+
+local function assert_cyclefree(luas)
+	local in_arcs = {}
+	for name, lua in pairs(luas) do
+		local ins = {}
+		for line in string.gmatch(lua, '([^\r\n]*)[\r\n]') do
+			local r = string.match(line, 'require%(?[\'"]([^\'")]*)[\'"]%)?')
+			if r then
+				ins[#ins + 1] = r
+			end
+		end
+		in_arcs[name] = ins
+	end
+
+	local seen = {}
+	for name in pairs(luas) do
+		dfs(name, in_arcs, function(name)
+			if seen[name] then
+				return false
+			end
+			seen[name] = true
+			return true
+		end, function(stack)
+			local min_idx = 1
+			local min = stack[1]
+			for i = 2, #stack do
+				local curr = stack[i]
+				if curr < min then
+					min = curr
+					min_idx = i
+				end
+			end
+
+			local pretty_cycle = { min }
+			local curr_idx = min_idx + 1
+			while curr_idx ~= min_idx do
+				pretty_cycle[#pretty_cycle + 1] = stack[curr_idx]
+
+				if curr_idx > #stack then
+					curr_idx = 1
+				else
+					curr_idx = curr_idx + 1
+				end
+			end
+			pretty_cycle[#pretty_cycle + 1] = pretty_cycle[1]
+
+			die('cycle detected:\n\t' .. table.concat(pretty_cycle, '\n\t -> '))
+		end)
+	end
+end
+
+local function lint(module, lua, test)
 	local options = {
 		globals = {
 			em = {
@@ -83,29 +220,94 @@ function lint(module_name, lua)
 					error = {},
 					warn = {},
 					observe = {},
-				}
-			}
-		}
+				},
+			},
+		},
 	}
 
+	if test then
+		options.globals.__tests = {}
+	end
+
 	local report = luacheck.get_report(lua)
-	local issues = luacheck.process_reports({report}, options)[1]
+	local issues = luacheck.process_reports({ report }, options)[1]
 
 	local messages = {}
 	for i = 1, #issues do
 		local issue = issues[i]
 		local msg = luacheck.get_message(issue)
 
-		messages[#messages + 1] = string.format("luacheck: error[%d]: %s:%d:%d-%d: %s", issue.code, module_name, issue.line, issue.column, issue.end_column, msg)
+		messages[#messages + 1] = string.format(
+			'luacheck: error[%d]: %s:%d:%d-%d: %s',
+			issue.code,
+			module,
+			issue.line,
+			issue.column,
+			issue.end_column,
+			msg
+		)
 	end
-
 	if #messages > 0 then
 		die(table.concat(messages, '\n'))
 	end
 end
 
-function encode(lua)
-	return { string.byte(lua, 1, #lua) }
+local function check(_asts, luas, test)
+	for module, lua in pairs(luas) do
+		lint(module, lua, test)
+	end
 end
 
-return compile(module_name, raw, test)
+local function encode(luas, test)
+	local buf = {}
+
+	if test then
+		buf[#buf + 1] = 'local __tests = {}\n'
+	end
+
+	local modules = {}
+	for name, _ in pairs(luas) do
+		modules[#modules + 1] = name
+	end
+	table.sort(modules)
+
+	for i = 1, #modules do
+		local module = modules[i]
+		buf[#buf + 1] = 'package.preload["'
+		buf[#buf + 1] = module
+		buf[#buf + 1] = '"] = function()\n'
+		buf[#buf + 1] = luas[module]
+		buf[#buf + 1] = 'end\n'
+	end
+
+	if test then
+		buf[#buf + 1] = 'package.preload["emtest"] = function()\n'
+		for i = 1, #modules do
+			buf[#buf + 1] = '\trequire("' .. modules[i] .. '")\n'
+		end
+		buf[#buf + 1] = '\tfor i = 1, #__tests do\n'
+		buf[#buf + 1] = '\t\t__tests[i]()\n'
+		buf[#buf + 1] = 'end\n'
+	end
+
+	local code = table.concat(buf)
+	if not test then
+		code = string.dump(load(code))
+	end
+
+	return { string.byte(code, 1, #code) }
+end
+
+local function compile(inputs, test)
+	local inputs = prepare(inputs)
+
+	local luas = luas_of(inputs)
+	local asts = asts_of(inputs)
+
+	assert_cyclefree(luas)
+	check(asts, luas, test)
+
+	return encode(luas, test)
+end
+
+return compile(inputs, test)
--- crates/yuescript/src/lib.rs
+++ crates/yuescript/src/lib.rs
@@ -1,9 +1,11 @@
 use std::{
+    collections::HashMap,
     env,
     error::Error,
     fs::{self, File},
     io::{BufWriter, Write},
-    path::{self, Path, PathBuf}, process::{self, ExitCode}
+    path::{self, Path, PathBuf},
+    process,
 };
 // use thiserror::Error;
 use mlua::{lua_State, Lua, Value};
@@ -13,20 +15,20 @@ extern "C" {
 }
 
 pub struct Compiler {
-    in_dir: PathBuf,
-    out_dir: PathBuf,
+    in_dir_path: PathBuf,
+    out_path: PathBuf,
     test: bool,
 }
 
 impl Compiler {
-    pub fn new<S, T>(in_dir: S, out_dir: T) -> Self
+    pub fn new<S, T>(in_dir: S, out_path: T) -> Self
     where
         S: Into<PathBuf>,
         T: Into<PathBuf>,
     {
         Self {
-            in_dir: in_dir.into(),
-            out_dir: out_dir.into(),
+            in_dir_path: in_dir.into(),
+            out_path: out_path.into(),
             test: false,
         }
     }
@@ -53,68 +55,66 @@ impl Compiler {
         let yue = unsafe { lua.create_c_function(luaopen_yue)? };
         lua.load_from_function::<_, Value>("yue", yue)?;
 
-        for input_path in input_paths.iter() {
-            let pretty_path = input_path.as_os_str().to_string_lossy();
-            println!("cargo:rerun-if-changed={pretty_path}");
-            println!("{}", std::env::current_dir()?.display());
-
-            let module_name = input_path
-                .strip_prefix(&self.in_dir)?
-                .with_extension("")
-                .as_os_str()
-                .to_string_lossy()
-                .replace(path::MAIN_SEPARATOR_STR, ".");
-
-            let globals = lua.globals();
-            globals.set("raw", fs::read_to_string(input_path)?)?;
-            globals.set("test", self.test)?;
-            globals.set("module_name", module_name.as_str())?;
-
-            let bytecode: Vec<u8> = match lua
-                .load(&compile_script)
-                .set_name(format!("compile:{module_name}"))?
-                .call(()) {
-                Ok(b) => b,
-                Err(e) => {
-                    eprintln!("{e}");
-                    process::exit(1);
-                }
-            };
-
-            println!("Creating file {:?}", self.output_path(&input_path));
-            let output_path = self.output_path(&input_path)?;
-            if let Some(parent) = output_path.parent() {
-                fs::create_dir_all(parent)?;
+        let globals = lua.globals();
+        globals.set("test", self.test)?;
+        globals.set("luacheck_path", self.luacheck_path()?)?;
+        globals.set("inputs", {
+            let tab = lua.create_table_with_capacity(0, input_paths.len() as i32)?;
+            for input_path in &input_paths {
+                let module_name = input_path
+                    .strip_prefix(&self.in_dir_path)?
+                    .with_extension("")
+                    .as_os_str()
+                    .to_string_lossy()
+                    .replace(path::MAIN_SEPARATOR, ".");
+                tab.set(module_name, fs::read_to_string(input_path)?)?;
             }
-            let mut out_file = BufWriter::new(File::create(output_path)?);
-            out_file.write_all(&bytecode)?;
+            tab
+        })?;
+
+        let bytecode: Vec<u8> = match lua.load(&compile_script).set_name("compile")?.call(()) {
+            Ok(b) => b,
+            Err(e) => {
+                eprintln!("{e}");
+                process::exit(1);
+            }
+        };
+
+        if let Some(parent) = self.out_path.parent() {
+            fs::create_dir_all(parent)?;
         }
+        let mut out_file = BufWriter::new(File::create(&self.out_path)?);
+        out_file.write_all(&bytecode)?;
 
         Ok(())
-
-        // input:
-        //     find files
-        //     figure out dependencies and hence load order
     }
 
     fn get_inputs(&self) -> Result<Vec<std::path::PathBuf>, Box<dyn Error>> {
         let mut to_compile = Vec::new();
-        for entry in fs::read_dir(&self.in_dir)? {
+        for entry in fs::read_dir(&self.in_dir_path)? {
             let entry = entry?;
             let path = entry.path();
             if entry.metadata()?.is_file()
                 && path.extension().map(|e| e.to_string_lossy()) == Some("yue".into())
             {
+                println!("cargo:rerun-if-changed={}", path.display());
                 to_compile.push(path)
             }
         }
         Ok(to_compile)
     }
 
-    fn output_path(&self, input_path: &Path) -> Result<PathBuf, Box<dyn Error>> {
-        Ok(self
-            .out_dir
-            .join(input_path.strip_prefix(&self.in_dir)?)
-            .with_extension("luac"))
+    fn luacheck_path(&self) -> Result<String, Box<dyn Error>> {
+        let current_file = PathBuf::from(env::current_dir()?)
+            .ancestors()
+            .nth(2)
+            .unwrap()
+            .join(file!());
+        let crate_dir = current_file.ancestors().nth(2).unwrap();
+        Ok(format!(
+            "{}/luacheck/src/?.lua;{}/luacheck/src/?/init.lua",
+            crate_dir.display(),
+            crate_dir.to_string_lossy().replace("luacheck", "?")
+        ))
     }
 }
